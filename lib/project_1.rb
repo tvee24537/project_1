68fc06082855386b6f6d80fa4c43bc24f7cb1c68
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-15 22:28:15 +0000

    commenting out example codes

--- lib/project_1.rb
+++ lib/project_1.rb
@@ -5,8 +5,8 @@ require 'pry'
 require_relative "./project_1/version"
 
 # req change
-require_relative './daily_deal/deal'
-require_relative './daily_deal/cli'
+#require_relative './daily_deal/deal'
+#require_relative './daily_deal/cli'
 
 module Project1
   class Error < StandardError; end
