f9affea8787fb125841320abec3c80bbaf21d252
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-15 22:14:45 +0000

    added in example codes for reference

--- lib/project_1.rb
+++ lib/project_1.rb
@@ -1,4 +1,12 @@
-require "project_1/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./project_1/version"
+
+# req change
+require_relative './daily_deal/deal'
+require_relative './daily_deal/cli'
 
 module Project1
   class Error < StandardError; end
