4d8b5cc76d30a4bc009cb372c5e175c6de91ea66
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-15 22:22:16 +0000

    changing code in cli.rb

--- lib/project_1/cli.rb
+++ lib/project_1/cli.rb
@@ -1,8 +1,36 @@
 class Project1::CLI
   
-  def ex
-    puts "test"
+  def forecast
+    list_days
+    menu
+    goodbye
   end
+  
+  def list_days
+    puts "This Week's Forecast:"
+    @deals = DailyDeal::Deal.today
+    @deals.each.with_index(1) do |deal, i|
+      puts "#{i}. #{deal.name} - #{deal.price} - #{deal.availability}"
+    end
+  end
+  
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the deal you'd like more info on or type list to see the deals again or type exit:"
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        the_deal = @deals[input.to_i-1]
+        puts "#{the_deal.name} - #{the_deal.price} - #{the_deal.availability}"
+      elsif input == "list"
+        list_days
+      else
+        puts "Not sure what you want, type list or exit."
+      end
+    end
+  end
+  
   
 end
 
