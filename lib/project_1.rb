98f0c2bbdd0f9765b5eb71429a8ac50e5c5c5f66
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-15 23:20:30 +0000

    Updating project_1.rb with require_relatives

--- lib/project_1.rb
+++ lib/project_1.rb
@@ -3,10 +3,8 @@ require 'nokogiri'
 require 'pry'
 
 require_relative "./project_1/version"
-
-# req change
-#require_relative './daily_deal/deal'
-#require_relative './daily_deal/cli'
+require_relative './project_1/forecast'
+require_relative './project_1/cli'
 
 module Project1
   class Error < StandardError; end
