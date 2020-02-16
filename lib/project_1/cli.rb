cc68b4bbde1a074790bf1fd983b7171c01ef21ff
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-16 23:43:28 +0000

    adding in comments on how this work1

--- lib/project_1/cli.rb
+++ lib/project_1/cli.rb
@@ -1,11 +1,13 @@
 class Project1::CLI
   
+  #Guider method, gem will run in 3 parts mentioned under the forecast method.
   def forecast
     list_days
     menu
     goodbye
   end
   
+  #This method will list out the days currently displayed on weather.gov with numbers in front.
   def list_days
     puts "This Week's Forecast:"
     @days = Project1::Forecast.today
@@ -14,13 +16,14 @@ class Project1::CLI
     end
   end
   
+  #This method lets the user input numbers, list, and exit to interact with the gem to display more detail about the day, and deal with user error as well. 
   def menu
     input = nil
     while input != "exit"
       puts "Enter the number of the day you'd like more info on, type list to see the days again or type exit:"
       input = gets.strip.downcase
-
-      if input.to_i > 0 && input.to_i <= 9
+      #input can only be from 1-9 because there's only 9 scraper methods currently.
+      if input.to_i > 0 && input.to_i <= 9   
         the_days = @days[input.to_i-1]
         puts "#{the_days.name} - #{the_days.temp} - #{the_days.condition}"
       elsif input == "list"
