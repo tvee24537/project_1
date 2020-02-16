c05035756b15e11db41c910a10f336b274280926
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-16 22:58:58 +0000

    deleting unneeded module in project_1.rb

--- lib/project_1.rb
+++ lib/project_1.rb
@@ -5,8 +5,3 @@ require 'pry'
 require_relative "./project_1/version"
 require_relative './project_1/forecast'
 require_relative './project_1/cli'
-
-module Project1
-  class Error < StandardError; end
-  # Your code goes here...
-end
