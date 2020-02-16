c3f3abb4b2a2ea4b20f312fcf5f51f3e47605634
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-16 21:40:14 +0000

    fixing error attempt1

--- lib/project_1/forecast.rb
+++ lib/project_1/forecast.rb
@@ -2,7 +2,7 @@ class Project1::Forecast
   attr_accessor :name, :temp, :condition
   
   def self.today
-    # Scrape weather.gov for forecast inforamtion for current week.
+    # Scrape weather.gov for forecast inforamtion for current week and storing it in the days array.
     days = []
 
     days << self.scrape_cast
@@ -16,36 +16,36 @@ class Project1::Forecast
     
     day = self.new
     
-    day.name[1] = doc.search("p")[8].text
-    day.name[2] = doc.search("p")[12].text
-    day.name[3] = doc.search("p")[16].text
-    day.name[4] = doc.search("p")[20].text
-    day.name[5] = doc.search("p")[24].text
-    day.name[6] = doc.search("p")[28].text
-    day.name[7] = doc.search("p")[32].text
-    day.name[8] = doc.search("p")[36].text
-    day.name[9] = doc.search("p")[40].text
+    day.name = doc.search("p")[8].text
+    day.name = doc.search("p")[12].text
+    day.name = doc.search("p")[16].text
+    day.name = doc.search("p")[20].text
+    day.name = doc.search("p")[24].text
+    day.name = doc.search("p")[28].text
+    day.name = doc.search("p")[32].text
+    day.name = doc.search("p")[36].text
+    day.name = doc.search("p")[40].text
     
-    day.condition[1] = doc.search("p")[10].text
-    day.condition[2] = doc.search("p")[14].text
-    day.condition[3] = doc.search("p")[18].text
-    day.condition[4] = doc.search("p")[22].text
-    day.condition[5] = doc.search("p")[26].text
-    day.condition[6] = doc.search("p")[30].text
-    day.condition[7] = doc.search("p")[34].text
-    day.condition[8] = doc.search("p")[38].text
-    day.condition[9] = doc.search("p")[42].text
+    day.condition = doc.search("p")[10].text
+    day.condition = doc.search("p")[14].text
+    day.condition = doc.search("p")[18].text
+    day.condition = doc.search("p")[22].text
+    day.condition = doc.search("p")[26].text
+    day.condition = doc.search("p")[30].text
+    day.condition = doc.search("p")[34].text
+    day.condition = doc.search("p")[38].text
+    day.condition = doc.search("p")[42].text
 
     
-    day.temp[1] = doc.search("p")[11].text
-    day.temp[2] = doc.search("p")[15].text
-    day.temp[3] = doc.search("p")[19].text
-    day.temp[4] = doc.search("p")[23].text
-    day.temp[5] = doc.search("p")[27].text
-    day.temp[6] = doc.search("p")[31].text
-    day.temp[7] = doc.search("p")[35].text
-    day.temp[8] = doc.search("p")[39].text
-    day.temp[9] = doc.search("p")[43].text
+    day.temp = doc.search("p")[11].text
+    day.temp = doc.search("p")[15].text
+    day.temp = doc.search("p")[19].text
+    day.temp = doc.search("p")[23].text
+    day.temp = doc.search("p")[27].text
+    day.temp = doc.search("p")[31].text
+    day.temp = doc.search("p")[35].text
+    day.temp = doc.search("p")[39].text
+    day.temp = doc.search("p")[43].text
     
     
 #    day.name = doc.search("h2.main-title").text.strip
