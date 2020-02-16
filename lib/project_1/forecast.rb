69a578595d26bc37e3fd8d1f1a677b0c31fc947f
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-16 22:56:19 +0000

    deleting reference materials in forecast.rb

--- lib/project_1/forecast.rb
+++ lib/project_1/forecast.rb
@@ -25,15 +25,9 @@ class Project1::Forecast
     doc = Nokogiri::HTML(open(@@url))
     
     day = self.new
-    
     day.name = doc.search("p")[8].text
     day.condition = doc.search("p")[10].text
     day.temp = doc.search("p")[11].text
-
-#    day.name = doc.search("h2.main-title").text.strip
-#    day.temp = doc.search("a.wantone").first.attr("href").strip
-#    day.condition = doc.search("a.wantone").first.attr("href").strip
-
     day
   end
   
@@ -117,47 +111,3 @@ class Project1::Forecast
     day
   end
 end
-
-
-#example code for ref.
-#class DailyDeal::Deal
-#  attr_accessor :name, :price, :availability, :url
-
-#  def self.today
-    # Scrape woot and meh and then return deals based on that data
-#    self.scrape_deals
-#  end
-
-#  def self.scrape_deals
-#    deals = []
-
-#    deals << self.scrape_woot
-#    deals << self.scrape_meh
-
-#    deals
-#  end
-
-#  def self.scrape_woot
-#    doc = Nokogiri::HTML(open("https://woot.com"))
-
-#    deal = self.new
-#    deal.name = doc.search("h2.main-title").text.strip
-#    deal.price = doc.search("#todays-deal span.price").text.strip
-#    deal.url = doc.search("a.wantone").first.attr("href").strip
-#    deal.availability = true
-
-#    deal
-#  end
-
-#  def self.scrape_meh
-#    doc = Nokogiri::HTML(open("https://meh.com"))
-
-#    deal = self.new
-#    deal.name = doc.search("section.features h2").text.strip
-#    deal.price = doc.search("button.buy-button").text.gsub("Buy it.", "").strip
-#    deal.url = "https://meh.com"
-#    deal.availability = true
-
-#    deal
-#  end
-#end
